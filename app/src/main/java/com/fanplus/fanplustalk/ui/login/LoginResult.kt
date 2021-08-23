9303ef17989202a9ac3b404eed42b2fe95b08991
yh.choi <yh.choi@fanmaum.com>
2021-08-23 09:48:18 +0900

    초기 셋팅

---
+++ app/src/main/java/com/fanplus/fanplustalk/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.fanplus.fanplustalk.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
